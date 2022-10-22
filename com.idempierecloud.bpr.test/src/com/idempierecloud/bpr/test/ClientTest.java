597e9b82cac55b797a7f610a1f54b621509be493
zuhriutama <zuhriutama@gmail.com>
2022-10-22 19:35:29 +0700

    added unit test for BPR

---
+++ com.idempierecloud.bpr.test/src/com/idempierecloud/bpr/test/ClientTest.java
@@ -0,0 +1,16 @@
+package com.idempierecloud.bpr.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.compiere.util.DB;
+import org.junit.jupiter.api.Test;
+
+public class ClientTest extends AbstractTestCase {
+
+	@Test
+	public void test_belitang_client_exists() throws Exception{
+		String value = DB.getSQLValueString(getTrxName(), "SELECT value FROM AD_Client Where value=?", "Belitang");
+		assertEquals("Belitang", value);
+	}
+	
+}
