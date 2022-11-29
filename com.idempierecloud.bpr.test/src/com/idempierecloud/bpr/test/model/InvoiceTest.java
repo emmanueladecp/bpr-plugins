7cf96d4b289dc161a95a1f723df89cc03dd4f3d6
mashudwahyu <mashudwahyu04@gmail.com>
2022-11-29 15:48:31 +0700

    Ticket #request-001157 [BPR] Shipment -> Invoice (Customer)

--- com.idempierecloud.bpr.test/src/com/idempierecloud/bpr/test/model/InvoiceTest.java
+++ com.idempierecloud.bpr.test/src/com/idempierecloud/bpr/test/model/InvoiceTest.java
@@ -2,6 +2,8 @@ package com.idempierecloud.bpr.test.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
+
 import org.compiere.model.MInvoice;
 import org.compiere.model.MInvoiceLine;
 import org.compiere.model.MOrder;
@@ -22,7 +24,50 @@ public class InvoiceTest extends AbstractTestCase{
 	private static final int C_UOM_ID_Each=100;
 	private static final int BPR_ListFakturPajak_ID=1000000;
 	private static final int C_Tax_ID=1000000;
-	
+	private static final int AD_Org_ID_DEPO_Balikpapan=1000013;
+	private static final int C_BPartner_ID_ACAI=1000020;
+	private static final int C_BPartner_Location_ID_ACAI=1000013;
+	private static final int Product_ULTIMA25KG = 1000007;
+	private static final int C_UOM_ID_ZAK=1000018;
+
+
+	@Test
+	public void test_checkDocStatusShipment_BeforeComplete() throws Exception{
+		MInvoice invoice = new MInvoice(Env.getCtx(),0,getTrxName());
+		invoice.setAD_Org_ID(AD_Org_ID_DEPO_Balikpapan);
+		invoice.setIsSOTrx(true);
+		invoice.setC_DocTypeTarget_ID(C_DocType_ID_AR_Invoice);
+		invoice.setC_BPartner_ID(C_BPartner_ID_ACAI);
+		invoice.setC_BPartner_Location_ID(C_BPartner_Location_ID_ACAI);
+		invoice.setM_PriceList_ID(M_PriceList_ID_Sumatra_GT);
+		invoice.setPaymentRule("B");
+		invoice.setC_Currency_ID(C_Currency_ID_Rupiah);
+		invoice.setDateInvoiced(getLoginDate());
+		invoice.setC_PaymentTerm_ID(C_PaymentTerm_ID_Immediate);
+		invoice.set_ValueOfColumn("BPR_ListFakturPajak_ID", 1000000);
+		invoice.setDocStatus(invoice.DOCSTATUS_Drafted);
+		invoice.setDocAction(invoice.DOCACTION_Complete);
+		invoice.saveEx();
+		
+		assertEquals(C_BPartner_ID_ACAI, invoice.getC_BPartner_ID());
+		
+		MInvoiceLine line = new MInvoiceLine(Env.getCtx(), 0, getTrxName());
+		line.setAD_Org_ID(AD_Org_ID_DEPO_Balikpapan);
+		line.setC_Invoice_ID(invoice.get_ID());
+		line.setLine(10);
+		line.setM_InOutLine_ID(1000045);
+		line.setM_Product_ID(Product_ULTIMA25KG);
+		line.setQty(new BigDecimal(3));
+		line.setC_UOM_ID(C_UOM_ID_ZAK);
+		line.setC_Tax_ID(C_Tax_ID);
+		line.setPrice();
+		line.saveEx();
+		
+		invoice.processIt(MOrder.ACTION_Complete);
+		invoice.saveEx();
+		
+		assertEquals(MInvoice.DOCSTATUS_Invalid, invoice.DOCSTATUS_Invalid);
+	}
 	
 	@Test
 	public void test_check_faktur_beforecomplete() throws Exception{
@@ -33,6 +78,7 @@ public class InvoiceTest extends AbstractTestCase{
 		invoice.setC_BPartner_Location_ID(C_BPARTNER_LOCATION_ARI_SAPUTRA);
 		invoice.setM_PriceList_ID(M_PriceList_ID_Sumatra_GT);
 		invoice.setPaymentRule("B");
+		invoice.setIsSOTrx(true);
 		invoice.setC_Currency_ID(C_Currency_ID_Rupiah);
 		invoice.setDateInvoiced(getLoginDate());
 		invoice.setC_PaymentTerm_ID(C_PaymentTerm_ID_Immediate);
@@ -53,7 +99,7 @@ public class InvoiceTest extends AbstractTestCase{
 		line.setPrice();
 		line.saveEx();
 		
-		invoice.processIt(MOrder.ACTION_Complete);
+		invoice.processIt(MInvoice.ACTION_Complete);
 		invoice.saveEx();
 		
 		assertEquals(MInvoice.DOCSTATUS_Invalid, invoice.DOCSTATUS_Invalid);
@@ -90,7 +136,7 @@ public class InvoiceTest extends AbstractTestCase{
 		line.setPrice();
 		line.saveEx();
 		
-		invoice.processIt(MOrder.ACTION_Complete);
+		invoice.processIt(MInvoice.ACTION_Complete);
 		invoice.saveEx();
 		
 		assertEquals(MInvoice.DOCSTATUS_Completed, invoice.DOCSTATUS_Completed);
--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/CInvoiceEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/CInvoiceEvent.java
@@ -2,7 +2,9 @@ package com.idempierecloud.bpr.event;
 
 import org.adempiere.base.event.IEventTopics;
 import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.MInOut;
 import org.compiere.model.MInvoice;
+import org.compiere.model.MInvoiceLine;
 import org.compiere.model.PO;
 import org.compiere.util.CLogger;
 import org.compiere.util.DB;
@@ -25,8 +27,23 @@ public class CInvoiceEvent extends CustomEvent {
 			checkFaktur();
 		else if(event.getTopic().equals(IEventTopics.DOC_BEFORE_COMPLETE))
 			checkFakturPajak();
+			checkDocStatusShipment();
 	}
 
+	private void checkDocStatusShipment() {
+		if(!invoice.get_ValueAsBoolean("isSOTrx"))
+			return;
+		MInvoiceLine[] lines = invoice.getLines();
+		for(MInvoiceLine line:lines) {
+			MInOut shipment = (MInOut) line.getM_InOutLine().getM_InOut();
+			if(shipment.getDocStatus().equalsIgnoreCase("CO"))
+				return;
+			else if(shipment.getDocStatus().equalsIgnoreCase("CL"))
+				return;
+			else
+				throw new AdempiereException("Shipment Document No : "+shipment.getDocumentNo()+" pada Invoice Line No "+ line.getLine()+" Belum complete!");
+		}
+	}
 	private void checkFaktur() {
 		if(invoice.get_ValueAsInt("BPR_ListFakturPajak_ID")==0)
 			return;
