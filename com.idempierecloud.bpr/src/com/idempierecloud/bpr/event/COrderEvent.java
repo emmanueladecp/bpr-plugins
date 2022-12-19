4a8d0d1b67a2a31e1a18a7c0082e2ca7439ce754
mashudwahyu <mashudwahyu04@gmail.com>
2022-12-19 10:23:26 +0700

    (Penyesuaian) Ticket #request-001159 [BPR] C_Order IsSoTrx SO_CreditAvailable jika ganti c_bpartner maka akan set so available qty

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderEvent.java
@@ -33,13 +33,18 @@ public class COrderEvent extends CustomEvent{
 			checkTimbanganPO();
 			checkTimbanganNetAmt();
 			checkSalesRep();
+			setCreditAvailable();
 		}
 	}
 	private void setCreditAvailable() {
-		if(order.isSOTrx()) {
+		if(order.isSOTrx()) {//if sales order
 			if(order.getC_BPartner_ID()>0) {
-				BigDecimal SO_CreditAvaiable = order.getC_BPartner().getSO_CreditLimit().subtract(order.getC_BPartner().getSO_CreditUsed());
-				order.set_ValueOfColumn("SO_CreditAvailable", SO_CreditAvaiable);			}
+				if(order.getC_BPartner_ID()!=order.get_ValueAsInt("C_BPartnerSR_ID")) {//if document new or c_bpartner_id is change
+					BigDecimal SO_CreditAvailable = order.getC_BPartner().getSO_CreditLimit().subtract(order.getC_BPartner().getSO_CreditUsed());
+					order.set_ValueOfColumn("SO_CreditAvailable", SO_CreditAvailable);
+					order.set_ValueOfColumn("C_BPartnerSR_ID", order.getC_BPartner_ID());
+				}
+			}
 		}
 	}
 	
