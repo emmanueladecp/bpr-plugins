b9089a83233c050d704579293d524bec18de23ac
mashudwahyu <mashudwahyu04@gmail.com>
2023-01-17 01:37:18 +0700

    Ticket #request-001215 [BPR] Sales Order, Document Action

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderEvent.java
@@ -6,6 +6,7 @@ import java.math.RoundingMode;
 import org.adempiere.base.event.IEventTopics;
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MBPartner;
+import org.compiere.model.MInOut;
 import org.compiere.model.MOrder;
 import org.compiere.model.MOrderLine;
 import org.compiere.model.PO;
@@ -42,16 +43,32 @@ public class COrderEvent extends CustomEvent{
 			setCreditUseBP();
 		}else if(event.getTopic().equals(IEventTopics.DOC_BEFORE_REACTIVATE)) {
 			resetQtyReserved();
+			checkshipment();
 		}else if(event.getTopic().equals(IEventTopics.DOC_BEFORE_VOID)) {
 			resetQtyReserved();
 			updatePOReference();
 			resetCreditUsed();
+			checkshipment();
 		}else if(event.getTopic().equals(IEventTopics.DOC_BEFORE_REVERSECORRECT)) {
 			resetQtyReserved();
 			updatePOReference();
 		}
 	}
 	
+	private void checkshipment() {
+		MOrderLine[] lines = order.getLines();
+		for(MOrderLine line:lines) {
+			int m_inout_id = DB.getSQLValue(line.get_TrxName(), "select mi2.m_inout_id from m_inoutline mi "
+					+ " join m_inout mi2 on mi.m_inout_id = mi2.m_inout_id "
+					+ " where mi2.docstatus in ('CO') and mi.c_orderline_id = ?", line.getC_OrderLine_ID());
+			MInOut shipment = new MInOut(line.getCtx(), m_inout_id, line.get_TrxName());
+			if(m_inout_id > 0) {
+				throw new AdempiereException("Order ini telah digunakan Shipment/Receipt : "+shipment.getDocumentNo());
+			}
+		}
+		
+	}
+
 	private void resetCreditUsed() {
 		if(order.isSOTrx()) {
 			MBPartner bp = (MBPartner) order.getC_BPartner();
