4b5b87b8543809a66897706f45cf988a1a56d6bb
Roynaldo <roynaldo.lim@gmail.com>
2023-02-17 10:08:50 +0700

    Payment Allocate Multi Charge

---
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/model/MPaymentExt.java
@@ -0,0 +1,130 @@
+package com.idempierecloud.bpr.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.MAllocationHdr;
+import org.compiere.model.MAllocationLine;
+import org.compiere.model.MPayment;
+import org.compiere.model.MPaymentAllocate;
+import org.compiere.process.DocAction;
+import org.compiere.process.DocumentEngine;
+import org.compiere.util.Env;
+import org.compiere.util.Msg;
+
+public class MPaymentExt extends MPayment {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8699346227246789924L;
+
+	public MPaymentExt(Properties ctx, int C_Payment_ID, String trxName) {
+		super(ctx, C_Payment_ID, trxName);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MPaymentExt(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 	Allocate It.
+	 * 	Only call when there is NO allocation as it will create duplicates.
+	 * 	If an invoice exists, it allocates that 
+	 * 	otherwise it allocates Payment Selection.
+	 *	@return true if allocated
+	 */
+	@Override
+	public boolean allocateIt()
+	{
+		//	Create invoice Allocation -	See also MCash.completeIt
+		if (getC_Invoice_ID() != 0)
+		{	
+				return allocateInvoice();
+		}	
+		//	Invoices of a AP Payment Selection
+		if (allocatePaySelection())
+			return true;
+		
+		if (getC_Order_ID() != 0)
+			return false;
+			
+		//	Allocate to multiple Payments based on entry
+		MPaymentAllocate[] pAllocs = MPaymentAllocate.get(this);
+		if (pAllocs.length == 0)
+			return false;
+		
+		MAllocationHdr alloc = new MAllocationHdr(getCtx(), false, 
+			getDateTrx(), getC_Currency_ID(), 
+				Msg.translate(getCtx(), "C_Payment_ID")	+ ": " + getDocumentNo(), 
+				get_TrxName());
+		alloc.setAD_Org_ID(getAD_Org_ID());
+		alloc.setDateAcct(getDateAcct()); // in case date acct is different from datetrx in payment; IDEMPIERE-1532 tbayen
+		if (!alloc.save())
+		{
+			log.severe("P.Allocations not created");
+			return false;
+		}
+		//	Lines
+		BigDecimal allocationAmtToCharge = Env.ZERO;
+		for (int i = 0; i < pAllocs.length; i++)
+		{
+			MPaymentAllocate pa = pAllocs[i];
+
+			BigDecimal allocationAmt = pa.getAmount();			//	underpayment
+			if (pa.getOverUnderAmt().signum() < 0 && pa.getAmount().signum() > 0)
+				allocationAmt = allocationAmt.add(pa.getOverUnderAmt());	//	overpayment (negative)
+
+			MAllocationLine aLine = null;
+			if (isReceipt())
+				aLine = new MAllocationLine (alloc, allocationAmt,
+					pa.getDiscountAmt(), pa.getWriteOffAmt(), pa.getOverUnderAmt());
+			else
+				aLine = new MAllocationLine (alloc, allocationAmt.negate(),
+					pa.getDiscountAmt().negate(), pa.getWriteOffAmt().negate(), pa.getOverUnderAmt().negate());
+			//aLine.setDocInfo(pa.getC_BPartner_ID(), 0, pa.getC_Invoice_ID());
+			if (pa.getC_Invoice_ID() != 0) {
+				aLine.setDocInfo(pa.getC_BPartner_ID(), 0, pa.getC_Invoice_ID());
+				aLine.setPaymentInfo(getC_Payment_ID(), 0);
+			}
+			else {
+				MPayment payment = new MPayment(pa.getCtx(), pa.getC_Payment_ID(), pa.get_TrxName());
+				aLine.setAmount(isReceipt() ? allocationAmt.negate() : allocationAmt);
+				aLine.setC_BPartner_ID(payment.getC_BPartner_ID());
+				aLine.setC_Charge_ID(pa.get_ValueAsInt("C_Charge_ID"));
+				
+				allocationAmtToCharge = allocationAmtToCharge.add(pa.getAmount());
+			}
+			if (!aLine.save(get_TrxName()))
+				log.warning("P.Allocations - line not saved");
+			else
+			{
+				pa.setC_AllocationLine_ID(aLine.getC_AllocationLine_ID());
+				pa.saveEx();
+			}
+		}
+		
+		if (allocationAmtToCharge.signum()!=0) {
+			MAllocationLine aLine = new MAllocationLine(alloc, isReceipt() ? allocationAmtToCharge : allocationAmtToCharge.negate(), null, null, null);
+			aLine.setC_BPartner_ID(getC_BPartner_ID());
+			aLine.setPaymentInfo(getC_Payment_ID(), 0);
+			if (!aLine.save(get_TrxName())) {
+				log.warning("P.Allocations Charge - line not saved");
+			}
+		}
+		
+		//do not post immediate alloc, alloc should post after payment
+		alloc.set_Attribute(DocumentEngine.DOCUMENT_POST_IMMEDIATE_AFTER_COMPLETE, Boolean.FALSE);
+		// added AdempiereException by zuhri
+		if (!alloc.processIt(DocAction.ACTION_Complete))
+			throw new AdempiereException(Msg.getMsg(getCtx(), "FailedProcessingDocument") + " - " + alloc.getProcessMsg());
+		addDocsPostProcess(alloc);
+		// end added
+		m_processMsg = "@C_AllocationHdr_ID@: " + alloc.getDocumentNo();
+		return alloc.save(get_TrxName());
+	}	//	allocateIt	
+}
