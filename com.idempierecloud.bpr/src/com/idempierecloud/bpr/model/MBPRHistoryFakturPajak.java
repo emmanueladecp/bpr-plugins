b991b5a6b484985fc58892ebe898660cafe8e917
zuhriutama <zuhriutama@gmail.com>
2023-02-20 09:21:40 +0700

    Ticket #request-001246 [BPR] bug nomor faktur di invoice

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/model/MBPRHistoryFakturPajak.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/model/MBPRHistoryFakturPajak.java
@@ -30,7 +30,7 @@ public class MBPRHistoryFakturPajak extends X_BPR_HistoryFakturPajak {
 	public static MBPRHistoryFakturPajak addHistory(MInvoice invoice, MBPRListFakturPajak pajak) {
 		MBPRHistoryFakturPajak history = new MBPRHistoryFakturPajak(invoice.getCtx(), 0, invoice.get_TrxName());
 		history.setC_Invoice_ID(invoice.getC_Invoice_ID());
-		history.setBPR_HistoryFakturPajak_ID(pajak.getBPR_ListFakturPajak_ID());
+		history.setBPR_ListFakturPajak_ID(pajak.getBPR_ListFakturPajak_ID());
 		history.setDescription(invoice.get_ValueAsString("TypePajak")+"."+pajak.getName());
 		history.saveEx();
 		return history;
