4de97aebad991396100297f98b347206186f6c90
mashudwahyu <mashudwahyu04@gmail.com>
2023-08-01 10:18:07 +0700

    Ticket #request-001329 [BPR] Validasi Cost Product sebelum complete Doc

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/MMovementEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/MMovementEvent.java
@@ -76,22 +76,22 @@ public class MMovementEvent extends CustomEvent {
 		for(MMovementLine line : movement.getLines(true)) {
 			BigDecimal MCost_CurrentCostPrice = DB.getSQLValueBD(line.get_TrxName(), "SELECT Coalesce(M_Cost.currentcostprice,0) FROM M_Cost WHERE AD_Org_ID = ? "+
 			 " and M_Product_ID = ? and M_CostElement_ID=?",movement.getAD_Org_ID(),line.getM_Product_ID(), M_CostElement_ID_AveragePO);
-			if(MCost_CurrentCostPrice.compareTo(BigDecimal.ZERO)>0) {
+			
+			if(MCost_CurrentCostPrice == null) {
+				throw new AdempiereException("Tidak ditemukan Cost untuk Product : "+line.getM_Product().getName()
+						+", Organization :  "+line.getAD_Org_ID()
+						+", Cost Elemet : Average PO");
+			}
+			else if(MCost_CurrentCostPrice.compareTo(BigDecimal.ZERO)>0) {
 				if(MCost_CurrentCostPrice.compareTo(BigDecimal.valueOf(0.001))>0) {
 					log.fine("Found Product Cost");
 				}else {
 					throw new AdempiereException("Cost untuk Product : "+line.getM_Product().getName()
 							+", Organization :  "+line.getAD_Org_ID()
 							+", Cost Elemet : Average PO, Current Cost Price Harus Lebih Besar dari 0.001");
-				}
-				
-			}else {
-				throw new AdempiereException("Tidak ditemukan Cost untuk Product : "+line.getM_Product().getName()
-											+", Organization :  "+line.getAD_Org_ID()
-											+", Cost Elemet : Average PO");
+				}				
 			}
 		}
-		
 	}
 
 	private void checkConfirmMovement() {
