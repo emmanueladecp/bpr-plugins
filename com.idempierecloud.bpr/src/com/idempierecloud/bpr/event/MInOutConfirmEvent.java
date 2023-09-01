f18a0de6dbc9ff8dd13c9d4257e477ed39882029
mashudwahyu <mashudwahyu04@gmail.com>
2023-09-01 14:49:09 +0700

    [BUG FIX] condition reject 1 line full

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/MInOutConfirmEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/MInOutConfirmEvent.java
@@ -216,13 +216,11 @@ public class MInOutConfirmEvent extends CustomEvent {
 					mline.setM_Movement_ID(movement.getM_Movement_ID());
 					mline.setM_Product_ID(shipLine.getM_Product_ID());
 					mline.setM_Locator_ID(shipLine.getM_Locator_ID());
-					if(line.getDifferenceQty().compareTo(line.getTargetQty())<0) {
-						int MLocator_Retur = DB.getSQLValue(line.get_TrxName(), "Select * from M_Locator where  M_LocatorType_ID=1000004 and M_Warehouse_ID=?", shipment.getM_Warehouse_ID());
-						if(MLocator_Retur>0)
-							mline.setM_LocatorTo_ID(MLocator_Retur);
-						else 
-							throw new AdempiereException("Locator Retur tidak ditemukan");
-					}
+					int MLocator_Retur = DB.getSQLValue(line.get_TrxName(), "Select coalesce(min(M_Locator_ID),0)  from M_Locator where  M_LocatorType_ID=1000004 and M_Warehouse_ID=?", shipment.getM_Warehouse_ID());
+					if(MLocator_Retur>0)
+						mline.setM_LocatorTo_ID(MLocator_Retur);
+					else 
+						throw new AdempiereException("Locator Retur tidak ditemukan");
 					mline.setMovementQty(line.getDifferenceQty());
 					mline.set_ValueOfColumn("timbangannetamt", BigDecimal.ZERO);
 					mline.set_ValueOfColumn("m_inoutline_id", shipLine.getM_InOutLine_ID());
