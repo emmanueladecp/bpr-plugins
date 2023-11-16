91798cf689e50d8bdbe6e363435ab4a48c6aea92
mashudwahyu <mashudwahyu04@gmail.com>
2023-11-16 14:59:00 +0700

    Ticket #request-001159 [BPR] C_Order IsSoTrx SO_CreditAvailable (additional filter SO status close)

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderLineEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/COrderLineEvent.java
@@ -254,7 +254,10 @@ public class COrderLineEvent extends CustomEvent {
 	}
 
 	private void checkSOCreditLimit() {
-		if(orderLine.getC_Order().isSOTrx()&&!orderLine.getC_Order().getDocStatus().equals("CO")) {
+        if(orderLine.getC_Order().isSOTrx()&&(!orderLine.getC_Order().getDocStatus().equals("CO"))) {
+            if(orderLine.getC_Order().getDocStatus().equals("CL")){
+                return;
+            }
 			if(orderLine.getC_Order().getC_DocTypeTarget_ID()!=1000084) {//proposal retur
 				MOrder order = (MOrder) orderLine.getC_Order();
 				BigDecimal lineamt = orderLine.getLineNetAmt();
