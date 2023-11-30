82873f54a7d1311c4d2a4205d48f09ab039bcfde
mashudwahyu <mashudwahyu04@gmail.com>
2023-11-30 13:14:52 +0700

    Set Faktur Pajak Invoice [Ticket #request-001373 [BPR] Process Penomeran Faktur Pajak]

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/process/SetFakturPajakInvoice.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/process/SetFakturPajakInvoice.java
@@ -3,10 +3,12 @@ package com.idempierecloud.bpr.process;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.logging.Level;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MInvoice;
+import org.compiere.process.ProcessInfoParameter;
 import org.compiere.util.DB;
 
 import com.idempierecloud.bpr.base.CustomProcess;
@@ -15,10 +17,20 @@ import com.idempierecloud.bpr.model.MBPRListFakturPajak;
 
 public class SetFakturPajakInvoice extends CustomProcess{
 
+	
+	String DocNoInv = null;
+	int Count = 0;
 	@Override
 	protected void prepare() {
-		// TODO Auto-generated method stub
-		
+		ProcessInfoParameter[] para = getParameter();
+		for (int i = 0; i < para.length; i++)
+		{
+			String name = para[i].getParameterName();
+			if (name.equals("DocumentNo"))
+				DocNoInv = para[i].getParameterAsString();
+			else
+				log.log(Level.SEVERE, "Unknown Parameter: " + name);
+		}
 	}
 
 	@Override
@@ -26,12 +38,18 @@ public class SetFakturPajakInvoice extends CustomProcess{
 		StringBuilder sql = new StringBuilder ("select ci.c_invoice_id, ci.documentno from c_invoice ci "
 				+ "	where ci.issotrx = 'Y' and c_doctypetarget_id <> 1000004 and ci.docstatus in ('CO') "
 				+ "	and ci.bpr_historyfakturpajak_id is null and ci.bpr_listfakturpajak_id is null and tax_no is null "
-				+ "	and ad_client_id = 1000003;");
+				+ "	and ad_client_id = 1000003");
+        if(DocNoInv!= null) {
+			sql.append(" and ci.documentno = ?");
+		}
+		
 		PreparedStatement pstmnt = null;
 		ResultSet rsl = null;
 		try
 		{
 			pstmnt = DB.prepareStatement (sql.toString(), get_TrxName());
+			if(!DocNoInv.equals(null))
+				pstmnt.setString(1, DocNoInv);
 			rsl = pstmnt.executeQuery ();
 			while (rsl.next ()){
 				MInvoice invoice = new MInvoice(getCtx(),rsl.getInt(1), get_TrxName());
@@ -46,6 +64,9 @@ public class SetFakturPajakInvoice extends CustomProcess{
 				invoice.set_ValueOfColumn(MBPRHistoryFakturPajak.COLUMNNAME_BPR_HistoryFakturPajak_ID, history.getBPR_HistoryFakturPajak_ID());
 				invoice.set_ValueOfColumn("tax_no", history.getDescription());
 				invoice.saveEx();
+				
+				Count++;
+				
 			}
 		}
 		catch (SQLException e){
@@ -57,7 +78,7 @@ public class SetFakturPajakInvoice extends CustomProcess{
 			pstmnt = null;
 		}
 		
-		return null;
+		return "Count Generate Faktur Invoice : "+Count;
 	}
 	
 
