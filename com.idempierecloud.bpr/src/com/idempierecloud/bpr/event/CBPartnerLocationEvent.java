a8794efd185f91eef1a300d65e428318afa8343c
mashudwahyu <mashudwahyu04@gmail.com>
2024-04-18 17:41:43 +0700

    Ticket #request-001442 [BPR] Set Location baseon bpLocation (Additional Village)

--- com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/CBPartnerLocationEvent.java
+++ com.idempierecloud.bpr/src/com/idempierecloud/bpr/event/CBPartnerLocationEvent.java
@@ -12,6 +12,7 @@ import org.osgi.service.event.Event;
 
 import com.idempierecloud.bpr.base.CustomEvent;
 import com.idempierecloud.bpr.model.X_BPR_District;
+import com.idempierecloud.bpr.model.X_BPR_Village;
 
 public class CBPartnerLocationEvent extends CustomEvent {
 
@@ -43,7 +44,7 @@ public class CBPartnerLocationEvent extends CustomEvent {
 			MRegion region = new MRegion(bpLocation.getCtx(), bpLocation.get_ValueAsInt("C_Region_ID"), bpLocation.get_TrxName());
 			MCity city = new MCity(bpLocation.getCtx(), bpLocation.get_ValueAsInt("C_City_ID"), bpLocation.get_TrxName());
 			X_BPR_District district = new X_BPR_District(bpLocation.getCtx(), bpLocation.get_ValueAsInt("bpr_district_id"), bpLocation.get_TrxName());			
-			MLocation loc = new MLocation(bpLocation.getCtx(), 0, bpLocation.get_TrxName());
+			MLocation loc = new MLocation(bpLocation.getCtx(), 0, bpLocation.get_TrxName());			
 			loc.setAddress1(bpLocation.getName());
 			loc.setAddress2(district.getName());
 			loc.setAddress4(city.getName());
@@ -51,6 +52,11 @@ public class CBPartnerLocationEvent extends CustomEvent {
 			loc.setC_Country_ID(bpLocation.get_ValueAsInt("C_Country_ID"));
 			loc.setC_Region_ID(bpLocation.get_ValueAsInt("C_Region_ID"));
 			loc.setC_City_ID(bpLocation.get_ValueAsInt("C_City_ID"));
+			if(bpLocation.get_ValueAsInt("BPR_Village_ID")>0) {
+				X_BPR_Village village = new X_BPR_Village(bpLocation.getCtx(), bpLocation.get_ValueAsInt("bpr_village_id"), bpLocation.get_TrxName());
+				loc.setAddress3(village.getName());
+				loc.set_ValueOfColumn("BPR_Village_ID", village.getBPR_Village_ID());
+			}	
 			loc.set_ValueOfColumn("BPR_District_ID", bpLocation.get_ValueAsInt("BPR_District_ID"));
 			loc.setIsActive(true);
 			loc.save();
